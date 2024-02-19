9ef5acf3d01bd539fbfcb25aa76938af315f3a75
nayeli <155994804+nayeli-guzman@users.noreply.github.com>
2024-02-19 18:54:36 -0500

    Update screen.h

--- screen.h
+++ screen.h
@@ -21,23 +21,23 @@ private:
 	bool selected_alg = false;
 public:
 
-	Screen() {}
+	Screen(){}
 
 	void execute() {
 
 		window.create({ NODE_SIZE * COLS
 						, NODE_SIZE * ROWS + HEADER_HEIGHT }
-		, "Project");
+					, "Project");
 
 		window.setFramerateLimit(60);
 
 		Grid grid(ROWS, COLS, window);
 
 		Menu menu(HEADER_HEIGHT
-			, NODE_SIZE * COLS
-			, NODE_SIZE * ROWS
-			, window
-		);
+					, NODE_SIZE * COLS
+					, NODE_SIZE * ROWS
+					, window
+			);
 
 
 		while (window.isOpen()) {
@@ -63,24 +63,25 @@ public:
 						if (event.mouseButton.button == Mouse::Left) {
 
 							// selected_alg : significa si es que el algoritmo ya ha sido mostrado en pantalla
+							// este lo cree para saber si el alg se mostraba en 
+							// pantalla, si era así limpiaba todo, menos inicio o fin
 							if (selected_alg) grid.partial_clean(1); // no se limpia fin, en la primera 
-							// iteración es false;
+														// iteración es false;
 							selected_alg = false;  // revisar el orden de estos dos if 
-
+							
 							if (grid.getEnd() == current_pos) // cuando inicio y fin se superponen, cuidado
-								//, en la primera iiteración getend = 0,0
+															//, en la primera iiteración getend = 0,0
 								grid.clearEnd(); // sus valores se ponen a -1,-1
-
+							
 							grid.setBegin(current_pos);
+							
 
-
-						}
-						else if (event.mouseButton.button == Mouse::Right) {
-
+						} else if (event.mouseButton.button == Mouse::Right) {
+							
 							if (selected_alg) grid.partial_clean(2);
-
+							
 							selected_alg = false;
-
+							
 							if (grid.getBegin() == current_pos)
 								grid.clearBegin();
 
@@ -98,70 +99,76 @@ public:
 
 						if (event.key.code == Keyboard::C) {
 							grid.total_clean(); // comienzo y fin se borran
+							selected_alg = false; // está todo limpio y no se muestra nada en pantalla
 						}
 
 						if (event.key.code >= Keyboard::Num1 && event.key.code <= Keyboard::Num3) {
-
+							
 							if (grid.isBegin() == false || grid.isEnd() == false) {
 								menu.changeDynamicText(1, 0); // 1: No begin or end
 							}
-
+							
 							else { // si el inicio y fin están definidos
 
 								if (event.key.code == Keyboard::Num1) {
+									if (selected_alg) grid.partial_clean(3);
+									selected_alg = false;
 									menu.updateSelector(1);
 									menu.changeDynamicText(2, 0); // 2. executing dijkstra
 									grid.setAlgorithm(1);
 								}
 
 								else if (event.key.code == Keyboard::Num2) {
+									if (selected_alg) grid.partial_clean(3);
+									selected_alg = false;
 									menu.updateSelector(2);
 									menu.changeDynamicText(3, 0); // 3. executing dfs
 									grid.setAlgorithm(2);
 								}
 
 								else if (event.key.code == Keyboard::Num3) {
+									if (selected_alg) grid.partial_clean(3);
+									selected_alg = false;
 									menu.updateSelector(3);
 									menu.changeDynamicText(4, 0); // 4. executing bfs
 									grid.setAlgorithm(3);
 								}
-
+							
 							}
 
 						}
-
+						
 						if (event.key.code == Keyboard::Space) {
-
+							
 							if (grid.getAlgorithm() != 0 && (grid.isBegin() == true && grid.isEnd() == true)) {
-
-								if (!selected_alg)
+								
+								if (!selected_alg) 
 									switch (grid.getAlgorithm()) {
-									case 1: // DIJKSTRA
-										// cuando ya se ejecutó y se muestra en pantalla
-										grid.dijkstra(menu);
-										grid.dijkstra_time();
-										menu.changeDynamicText(5, grid.getTime());
-										selected_alg = true;
-										break;
-									case 2: // DFS
-										grid.dfs(menu);
-										grid.dfs_time();
-										menu.changeDynamicText(6, grid.getTime());
-										selected_alg = true;
-										break;
-									case 3: // BFS
-										grid.bfs(menu);
-										grid.bfs_time();
-										menu.changeDynamicText(7, grid.getTime());
-										selected_alg = true;
-										break;
-									default:
-										break;
+										case 1: // DIJKSTRA
+											// cuando ya se ejecutó y se muestra en pantalla
+											grid.dijkstra(menu);
+											//grid.dijkstra_time();
+											menu.changeDynamicText(5, grid.getTime());
+											selected_alg = true;
+											break;
+										case 2: // DFS
+											grid.dfs(menu);
+											//grid.dfs_time();
+											menu.changeDynamicText(6, grid.getTime());
+											selected_alg = true;
+											break;
+										case 3: // BFS
+											grid.bfs(menu);
+											//grid.bfs_time();
+											menu.changeDynamicText(7, grid.getTime());
+											selected_alg = true;
+											break;
+										default:
+											break;
 									}
-
-							}
-							else menu.changeDynamicText(1, 0);
-
+							
+							} else menu.changeDynamicText(1,0);
+							
 						}
 
 					}
@@ -173,7 +180,7 @@ public:
 			grid.draw();
 			menu.draw();
 
-		}
+		}	
 
 	}
 
@@ -181,7 +188,7 @@ public:
 	Tuple getTuple() {
 
 		Vector2i mouse = Mouse::getPosition(window);
-
+		
 		int x = mouse.x / NODE_SIZE;
 		int y = mouse.y / NODE_SIZE - (HEADER_HEIGHT / NODE_SIZE);
 
