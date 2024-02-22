c261bb9da788d3a5abb4ff0ce67bce5f8a8c3b5d
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-02-22 12:03:32 -0500

    node.h

--- node.h
+++ node.h
@@ -5,10 +5,11 @@
 using namespace std;
 using namespace sf;
 
+template <class T = int>
 struct Tuple {
-	int x, y;
+	T x, y;
 
-	Tuple() : x(0), y(0){}
+	Tuple() : x(0), y(0) {}
 	Tuple(int x, int y) : x(x), y(y) {}
 	Tuple(const Tuple& t2) { x = t2.x; y = t2.y; }
 
@@ -25,24 +26,24 @@ struct Tuple {
 
 };
 
-class Node{
+class Node {
 private:
 	Sprite* square;
 	Texture* image;
 	Node** grid;
 	char type;
-	Tuple tuple;
+	Tuple<> tuple;
 	int x, y, number_grid = 2;
 
 
-	vector<Tuple> v_n; // para almacenar los vecinos del nodo
+	vector<Tuple<>> v_n; // para almacenar los vecinos del nodo
 
 	bool visited = false // para verificar si el nodo ya ha sido visitado
 		, queued = false // para verificar si el nodo está en la cola
 		, is_start = false // para verificar si el nodo es el comienzo
 		, is_end = false
-	; 
-	Tuple prior;
+		;
+	Tuple<> prior;
 	bool dijkstra = false;
 
 	int rows, cols;
@@ -51,9 +52,9 @@ private:
 
 public:
 
-	Node(){}
+	Node() {}
 
-	Node(Tuple t, char _t, int number) {
+	Node(Tuple<> t, char _t, int number) {
 
 		number_grid = number;
 
@@ -64,24 +65,24 @@ public:
 		square = new Sprite();
 
 		switch (number_grid) {
-			case 1:
-				node_size = 32;
-				break;
-			case 2:
-				node_size = 40;
-				break;
+		case 1:
+			node_size = 32;
+			break;
+		case 2:
+			node_size = 40;
+			break;
 		}
-		
+
 		square->setPosition(t.x * node_size
-						    , t.y * node_size + HEADER_HEIGHT
+			, t.y * node_size + HEADER_HEIGHT
 		);
 
 		image = new Texture();
-		type = _t;	
-		
+		type = _t;
+
 	}
 
-	void animation(Tuple t, char _t) {
+	void animation(Tuple<> t, char _t) {
 
 		type = _t;
 
@@ -93,7 +94,7 @@ public:
 			filterImages_x40();
 			break;
 		}
-		
+
 
 	}
 
@@ -128,12 +129,12 @@ public:
 			image->loadFromFile("sprites/x32/end_sprite.png");
 			square->setTexture(*image);
 			break;
-		
+
 		default:
 			break;
 		}
 
-		
+
 	}
 
 	void filterImages_x40() {
@@ -185,27 +186,27 @@ public:
 			break;
 		}
 
-		
+
 		w.draw(*square);
-		
+
 	}
 
 	// dijkstra
 
 	void set_n(Node** grid) {
 
-		
+
 		v_n.resize(0);
 
 		if (type != 'W') { // las paredes no pueden tener vecinos
 
 			dijkstra = true; // cada nodo está ejecutando dijkstra
 			// las paredes no pueden ser vecinos
-			if (x > 0 && grid[x - 1][y].type != 'W') v_n.push_back(Tuple(x - 1, y));
-			if (x < (cols - 1) && grid[x + 1][y].type != 'W') v_n.push_back(Tuple(x + 1, y));
+			if (x > 0 && grid[x - 1][y].type != 'W') v_n.push_back(Tuple<>(x - 1, y));
+			if (x < (cols - 1) && grid[x + 1][y].type != 'W') v_n.push_back(Tuple<>(x + 1, y));
 
-			if (y > 0 && grid[x][y - 1].type != 'W') v_n.push_back(Tuple(x, y - 1));
-			if (y < (rows - 1) && grid[x][y + 1].type != 'W') v_n.push_back(Tuple(x, y + 1));
+			if (y > 0 && grid[x][y - 1].type != 'W') v_n.push_back(Tuple<>(x, y - 1));
+			if (y < (rows - 1) && grid[x][y + 1].type != 'W') v_n.push_back(Tuple<>(x, y + 1));
 
 		}
 
@@ -218,7 +219,7 @@ public:
 			dijkstra = false;
 			queued = false;
 			visited = false;
-			Tuple prior;
+			Tuple<> prior;
 			v_n.clear();
 			type = 'E';
 		}
@@ -230,12 +231,12 @@ public:
 		if (dijkstra) {
 
 			dijkstra = false; // ya no estará ejecutando dijks
-			queued = false; 
+			queued = false;
 			visited = false;
-			Tuple prior;
+			Tuple<> prior;
 			v_n.clear();
 			type = 'E';
-			
+
 		}
 	}
 
@@ -252,11 +253,11 @@ public:
 
 	void setType(char c) {
 		type = c;
-		
-		
+
+
 	}
 
-	void setPrior(Tuple c) {
+	void setPrior(Tuple<> c) {
 		prior = c;
 	}
 
@@ -271,7 +272,7 @@ public:
 	void setRows(int c) {
 		rows = c;
 	}
-	
+
 	void setCols(int c) {
 		cols = c;
 	}
@@ -290,17 +291,16 @@ public:
 		return type;
 	}
 
-	Tuple getPrior() {
+	Tuple<> getPrior() {
 		return prior;
 	}
 
-	Tuple getTuple() {
+	Tuple<> getTuple() {
 		return tuple;
 	}
 
-	vector<Tuple> get_n() {
+	vector<Tuple<>> get_n() {
 		return v_n;
 	}
 
 };
-
